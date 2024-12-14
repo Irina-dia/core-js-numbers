70d8f51c10a883c9e8a8759956dc8456388d0d58
Irina-dia <dia.tework@gmail.com>
2024-12-15 00:51:15 +0300

    feat: added 5 new tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -204,8 +204,12 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n <= 1) return false;
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) return false;
+  }
+  return true;
 }
 
 /**
@@ -238,8 +242,8 @@ function toNumber(/* value, def */) {
  *   -2 => -8
  *   0  => 0
  */
-function getCube(/* num */) {
-  throw new Error('Not implemented');
+function getCube(num) {
+  return num ** 3;
 }
 
 /**
@@ -270,8 +274,12 @@ function getFibonacciNumber(/* index */) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  let a = 0;
+  for (let i = 0; i <= n; i += 1) {
+    a += i;
+  }
+  return a;
 }
 
 /**
@@ -285,8 +293,13 @@ function getSumToN(/* n */) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  let a = 0;
+  const numArr = num.toString().split('');
+  for (let i = 0; i < numArr.length; i += 1) {
+    a += Number(numArr[i]);
+  }
+  return a;
 }
 
 /**
